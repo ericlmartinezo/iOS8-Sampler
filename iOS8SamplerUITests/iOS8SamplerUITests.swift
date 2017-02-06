c2b23648b78e1f98bd2f37ab28c07247023142a6
Eric Martinez <ericlmartinez@gmail.com>
2017-02-06 01:24:46 -0800

    created test for first screen

--- iOS8SamplerUITests/iOS8SamplerUITests.swift
+++ iOS8SamplerUITests/iOS8SamplerUITests.swift
@@ -36,32 +36,61 @@ class iOS8SamplerUITests: XCTestCase {
     
     func testValidate() {
         _ = XCUIApplication()
-        
         XCUIApplication().tables.staticTexts["Demo using a data buffer to set uniforms for the vertex and fragment shaders."].tap()
         
     }
+
+
+//    func testa() {
+//    let app = XCUIApplication()
+//    app.tables.staticTexts["New filters of CIFilter such as CIGrassDistortion, CIDivideBlendMode, ..."].tap()
+//    app.navigationBars["New Image Filters"].buttons["iOS8 Sampler"].tap()
+//
+//  }
+//    func testAppb() {
+//    let app = XCUIApplication()
+//    app.tables.staticTexts["Distortion and Delay effect for audio using AVAudioEngine."].tap()
+//    app.navigationBars["Audio Effects"].buttons["iOS8 Sampler"].tap()
+//  }
+//    
+//    func testAppc() {
+//        let app = XCUIApplication()
+//        app.tables.staticTexts["Custom CIFilter examples using CIKernel."].tap()
+//        app.navigationBars["CustomFilters"].buttons["iOS8 Sampler"].tap()
+//    }
     
-    func testEffect() {
-        _ = XCUIApplication()
-        XCUIApplication().tables.staticTexts["Audio Effects"].tap()
+    func testAll() {
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
         
-    }
-    
-    func testNav() {
-    let app = XCUIApplication()
-    _ = app.tables
-    app.navigationBars["Audio Effects"].buttons["iOS8 Sampler"].tap()
-        
+        app.tables.staticTexts["Distortion and Delay effect for audio using AVAudioEngine."].tap()
+        app.navigationBars["Audio Effects"].buttons["iOS8 Sampler"].tap()
+        app.tables.staticTexts["New filters of CIFilter such as CIGrassDistortion, CIDivideBlendMode, ..."].tap()
+        app.navigationBars["New Image Filters"].buttons["iOS8 Sampler"].tap()
+        tablesQuery.staticTexts["CustomFilters"].tap()
+        app.navigationBars["CustomFilters"].buttons["iOS8 Sampler"].tap()
+        tablesQuery.staticTexts["Demo using a data buffer to set uniforms for the vertex and fragment shaders."].tap()
+        app.navigationBars["Metal Uniform Stream"].children(matching: .button).matching(identifier: "Back").element(boundBy: 0).tap()
+        tablesQuery.staticTexts["SceneKit"].tap()
+        app.navigationBars["SceneKit"].buttons["iOS8 Sampler"].tap()
+        //tablesQuery.staticTexts["HealthKit"].tap()
+        //app.navigationBars["HealthKit"].buttons["iOS8 Sampler"].tap()
+        tablesQuery.staticTexts["Touch ID"].tap()
+        app.navigationBars["Touch ID"].buttons["iOS8 Sampler"].tap()
+        tablesQuery.staticTexts["Metal Basic"].tap()
+        app.navigationBars["Metal Basic"].buttons["iOS8 Sampler"].tap()
+        tablesQuery.staticTexts["Invoke Touch ID verification using LocalAuthentication framework."].swipeUp()
+        tablesQuery.staticTexts["WebKit"].tap()
+        app.navigationBars["WebKit"].buttons["iOS8 Sampler"].tap()
+//        tablesQuery.staticTexts["Browsing Example using WKWebView"].swipeUp()
+//        tablesQuery.staticTexts["Pedometer"].tap()
+//        app.navigationBars["Pedometer"].buttons["iOS8 Sampler"].tap()
     }
 }
 
 
-//let app = XCUIApplication()
-//let tablesQuery = app.tables
-//tablesQuery.staticTexts["Distortion and Delay effect for audio using AVAudioEngine."].tap()
-//app.navigationBars["Audio Effects"].buttons["iOS8 Sampler"].tap()
-//
-//tablesQuery.staticTexts["New filters of CIFilter such as CIGrassDistortion, CIDivideBlendMode, ..."].tap()
-//app.navigationBars["New Image Filters"].buttons["iOS8 Sampler"].tap()
+
+
+
 
 
